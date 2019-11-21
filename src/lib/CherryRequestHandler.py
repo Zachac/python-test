b7608fabbbb6da2563cf5a89eb4ee875514c5906
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-11-21 10:29:28 -0600

    use request paramaters to add map options

--- src/lib/CherryRequestHandler.py
+++ src/lib/CherryRequestHandler.py
@@ -3,6 +3,7 @@ from lib.world.Map import Map
 
 class CherryRequestHandler(object):
     @cherrypy.expose
-    def map(self):
+    def map(self, x=0, y=0, size=10):
+        print(cherrypy.request.params)
         cherrypy.response.headers['Content-Type'] = 'text/plain'
-        return Map.getMap(0, 0)
+        return Map.getMap(int(x), int(y), int(size))
--- src/lib/world/Map.py
+++ src/lib/world/Map.py
@@ -8,14 +8,18 @@ class Map:
 
         if (height < 0):
             return "  "
-        else:
+        elif (height < 0.3):
             return "~ "
+        elif (height < 0.9):
+            return "# "
+        else:
+            return "/\\"
 
     @staticmethod
-    def getMap(x, y):
+    def getMap(x, y, size=10):
         result = ""
-        for i in range(x - 10, x + 10):
-            for j in range(y - 10, y + 10):
+        for i in range(x - size, x + size):
+            for j in range(y - size, y + size):
                 result += Map.icon(i, j)
             result += "\n"
         return result
