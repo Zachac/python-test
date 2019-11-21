b50859b7e8fd308ad98a14123a49b60dfb79366f
Zachary Chandler <zachary.a.chandler96@gmail.com>
2019-11-21 12:45:56 -0600

    add additional endpoint

--- src/lib/CherryRequestHandler.py
+++ src/lib/CherryRequestHandler.py
@@ -1,9 +1,15 @@
 import cherrypy
+import pprint
+from lib.World import World
 from lib.world.Map import Map
 
 class CherryRequestHandler(object):
     @cherrypy.expose
     def map(self, x=0, y=0, size=10):
-        print(cherrypy.request.params)
         cherrypy.response.headers['Content-Type'] = 'text/plain'
         return Map.getMap(int(x), int(y), int(size))
+
+    @cherrypy.expose
+    def locations(self):
+        cherrypy.response.headers['Content-Type'] = 'text/plain'
+        return pprint.pformat(World.locations)
